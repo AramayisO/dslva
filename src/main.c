476407e6ce6013a2895738f5750500212c520bab
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2024-04-12 00:23:42 -0700

    Simple program to check compile and flash works

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main() {
+    DDRB |= (1 << PINB5);
+
     while (1) {
+        PORTB |= (1 << PINB5);
+        _delay_ms(100);
+        PORTB &= ~(1 << PINB5);
+        _delay_ms(100);
     }
 
     return 0;
